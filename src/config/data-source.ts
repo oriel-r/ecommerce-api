5638645ec46263f2ef237591ee2a39e641d4f0b8
Oriel Romero <orielromero97@gmail.com>
2025-01-19 11:53:22 -0300

    The data-source config was moddified to use sqlite data base

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -6,20 +6,14 @@ import { registerAs } from '@nestjs/config';
 dotennConfig({path: '.env.development.local'})
 
 const dataSourceConfig: DataSourceOptions = {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT as unknown as number,
+        type: 'sqlite',
         database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
+        key: process.env.DB_PASSWORD
         entities:  ['dist/**/*.entity{.ts,.js}'],
         migrations: ['dist/migration/*{.ts,.js}'],
         logging: ['error'],
         synchronize: true,
         dropSchema: true,
-         extra: {
-                ssl: true
-         } 
 }
 
 export default registerAs('typeorm', () => dataSourceConfig)
